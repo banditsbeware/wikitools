71a7ed6e817067cdcb2fa03bf0a08815472e1592
david rademacher <david.rademacher@mavs.uta.edu>
2020-10-01 11:45:05 -0500

    catch up main

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from page import page
 from wikitools import *
+import re
 
 h = page()
 
-print(h)
-for p in h.htm('p'):
-	print(p.text)
+journey(page_rand(), 10)
+'''
+cat = cat_rand()
+pgs = cat_pages(cat, 10)
 
-
-# journey(page_rand(), 10)
-
-# cat = cat_rand()
-# pgs = cat_pages(cat, 10)
-
-# print(f'\n{cat}:')
-# for i,p in enumerate(pgs):
-# 	print(f'{i+1}. {p}')
+print(f'\n{cat}:')
+for i,p in enumerate(pgs):
+	print(f'{i+1}. {p}')
+'''
